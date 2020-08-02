d064795b639d6822f65cd6098a5dd1ce8fd55e0d
amacken <67441306+amacken@users.noreply.github.com>
2020-08-02 14:42:19 -0400

    Updated JS

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-console.log('Test')
+console.log('test')
 console.log($)
+
+$(() => {
+    const playerOneList = [];
+
+    const playerTwoList = [];
+
+    const renderOne = (arr) => {
+        const $ulOne = $('.theList');
+        for(let item of arr) {
+            const $liOne = $('<li class="listOne">');
+
+            $('#playerOnePicks' > 'h4' > playerOneList).addClass('theList')
+            $liOne.text(item);
+            $liOne.appendTo('#playerOnePicks');
+        }
+    }
+
+    $(() => {
+        $('form').on('submit', (event) => {
+            const inputValue = $('#input-box').val();
+            playerOneList.push(inputValue)
+            event.preventDefault();
+            $(event.currentTarget).trigger('reset');
+            renderOne(playerOneList)
+        })
+    })
+
+    
+
+})
