b2c6f421e1ac4cca40f66a852c0eee72a438340a
amacken <67441306+amacken@users.noreply.github.com>
2020-08-03 13:27:03 -0400

    Cleaned up JS prior to creating board 2

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 $(() => {
 
     const rows = 10;
-    const cols = 10;
+    const columns = 10;
     const squareSize = 50;
 
     const gameBoardContainer = document.getElementById("gameboard");
 
-    for (i = 0; i < cols; i++) {
+    for (i = 0; i < columns; i++) {
         for (j = 0; j < rows; j++) {
 
             let square = document.createElement("div");
@@ -44,26 +44,24 @@ $(() => {
     const attack = (event) => {
         if (event.target !== event.currentTarget) {
             let row = event.target.id.substring(1,2);
-            let col = event.target.id.substring(2,3);
-            //alert(`Clicked on row ${row}, col ${col}`)
-            if (gameBoard[row][col] === 0) {
-                event.target.style.background = '#bbb';
-                gameBoard[row][col] = 3;
-            } else if (gameBoard[row][col] === 1) {
+            let column = event.target.id.substring(2,3);
+    
+            if (gameBoard[row][column] === 0) {
+                event.target.style.background = 'blue';
+                gameBoard[row][column] = 3;
+            } else if (gameBoard[row][column] === 1) {
                 event.target.style.background = 'red';
-                gameBoard[row][col] = 2;
+                gameBoard[row][column] = 2;
                 hitCount++;
                 if (hitCount === 17) {
                     alert("All the enemy ships have been destroyed! You win!")
                 }
-            } else if (gameBoard[row][col] > 1) {
+            } else if (gameBoard[row][column] > 1) {
                 alert("You've already attacked there, try another location!")
             }
         }
     }
     
-    gameBoardContainer.addEventListener('click', attack, false);
-
-
+    gameBoardContainer.addEventListener('click', attack);
 
 })
